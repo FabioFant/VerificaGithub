f788a38f547421960f7fe226189b16e5ab8b52a8
Fabio Fantini <FABIO.FANTINI@local.ittcesena.it>
2024-10-21 08:36:43 +0200

    Release Libro 1: Aggiunta e terminata classe Libro

---
+++ WpfAppBiblioteca/WpfAppBiblioteca/Libro.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppBiblioteca
+{
+    class Libro
+    {
+        private string _autore;
+        private string _titolo;
+        private int _annoDiPubblicazione;
+        private string _editore;
+        private int _numeroDiPagine;
+
+        private string Autore { get { return _autore; } }
+        private string Titolo { get { return _titolo; } }
+        private int AnnoDiPubblicazione { get { return _annoDiPubblicazione; } }
+        private string Editore { get { return _editore; } }
+        private int NumeroDiPagine { get { return _numeroDiPagine; } }
+
+        public Libro(string autore, string titolo, int annoDiPubblicazione, string editore, int numeroDiPagine)
+        {
+            _autore = autore;
+            _titolo = titolo;
+            _annoDiPubblicazione = annoDiPubblicazione;
+            _editore = editore;
+            _numeroDiPagine = numeroDiPagine;
+        }
+
+        public override string ToString()
+        {
+            return $"{Autore} {Titolo} {AnnoDiPubblicazione} {Editore} {NumeroDiPagine}";
+        }
+
+        public string ReadingTime()
+        {
+            if (NumeroDiPagine < 100)
+                return "1h";
+            else if (NumeroDiPagine >= 100 && NumeroDiPagine <= 200)
+                return "2h";
+            else if (NumeroDiPagine >= 200)
+                return "2h++";
+
+            return "errore";
+        }
+    }
+}
